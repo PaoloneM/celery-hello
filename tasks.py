77079d37aaef3dd00219123039f94e63ff19dec5
paolo <paolo.morgano@smartlis.com>
2021-07-22 12:41:46 +0200

    feat(app): add failover strategy

--- tasks.py
+++ tasks.py
@@ -1,14 +1,35 @@
 from celery import Celery
 import time
+import itertools
+import random
+from datetime import datetime
 
-broker_url = [
-    'amqp://guest:guest@localhost:5672//',
-    'amqp://guest:guest@localhost:6672//',
-    'amqp://guest:guest@localhost:7672//',
+def random_failover_strategy(servers):
+    it = list(servers)  # don't modify callers list
+    random.shuffle(it)
+    for i in itertools.cycle(it):
+        yield i
+
+BROKER_URL = [
+    "amqp://guest:guest@localhost:5672//",
+    "amqp://guest:guest@localhost:6672//",
+    "amqp://guest:guest@localhost:7672//"
 ]
+BROKER_FAILOVER_STRATEGY = random_failover_strategy
 
-app = Celery('tasks', backend='redis://localhost', broker=broker_url)
+# random.shuffle(BROKER_URL)
 
+app = Celery(	'proj', 
+#		backend='redis://localhost', 
+		broker=BROKER_URL,
+		broker_failover_strategy=BROKER_FAILOVER_STRATEGY,
+		task_default_queue="celery-%s" % datetime.now(),
+        	include=['proj.tasks'])
+
+# Optional configuration, see the application user guide.
+app.conf.update(
+    result_expires=3600,
+)
 @app.task
 def add(x, y):
     time.sleep(1)
