95f1095f237d7329b87d68ac13600eb67b65316d
paolo <paolo.morgano@smartlis.com>
2021-11-17 11:57:23 +0100

    add multiple brokers

--- proj/celery.py
+++ proj/celery.py
@@ -1,8 +1,29 @@
 from celery import Celery
+import itertools
+import random
+from datetime import datetime
+
+
+def random_failover_strategy(servers):
+    it = list(servers)  # don't modify callers list
+    random.shuffle(it)
+    for i in itertools.cycle(it):
+        yield i
+
+BROKER_URL = [
+    "amqp://guest:guest@localhost:5672//",
+    "amqp://guest:guest@localhost:6672//",
+    "amqp://guest:guest@localhost:7672//"
+]
+BROKER_FAILOVER_STRATEGY = random_failover_strategy
+
+random.shuffle(BROKER_URL)
 
 app = Celery(	'proj', 
 		backend='redis://localhost', 
-		broker='amqp://guest:guest@localhost//',
+		broker=BROKER_URL,
+		broker_failover_strategy=BROKER_FAILOVER_STRATEGY,
+		task_default_queue="celery-%s" % datetime.now(),
         	include=['proj.tasks'])
 
 # Optional configuration, see the application user guide.
